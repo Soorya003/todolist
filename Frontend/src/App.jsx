8b74f04582c174b221d673894e9e9e6e4bfc5a78
Soorya003 <sooryasoor003@gmail.com>
2024-11-18 11:12:59 +0530

    updated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Todo from './Todo'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='container'>
+    <Todo />
+    </div>
+     </>
+  )
+}
+
+export default App
